455086f5d0fa66168ba7c9ee1ec76fda6562c9d8
pmilos <pmilos@mimuw.edu.pl>
2021-03-19 16:28:42 +0100

    slurm n_tasks script

--- docker/run_remote_slurm.sh
+++ docker/run_remote_slurm.sh
@@ -13,11 +13,6 @@
 # limitations under the License.
 
 
-die () {
-    echo >&2 "$@"
-    exit 1
-}
-
 echo "run begins"
 pwd
 
@@ -26,13 +21,13 @@ EXPDIR=`pwd`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-echo "in DIR"
+echo "in DIR $DIR"
 
 ENVIRONMENT=$1
 AGENT=$2
-NUM_ACTORS=$3
-MRUNER_CONFIG=$5
-shift 5
+NUM_ACTORS=$SLURM_NTASKS
+MRUNER_CONFIG=$4
+shift 4
 
 echo "args read"
 
@@ -42,34 +37,44 @@ cd $DIR
 
 echo "PYTHONPATH updated"
 
-#source /home/mizaw/venvs/py38_seedrl/bin/activate
-
 export PYTHONPATH=$PYTHONPATH:/
-
+# set random seed common across tasks
+RANDOM=$SLURM_OB_ID
 NONCE=id$RANDOM$RANDOM$RANDOM
+#echo "nonce created"
 
-echo "nonce created"
+RANDOM_PORT=$((49152 + RANDOM % (65535 - 49152)))
+SERVER_ADDRESS_LOCAL="localhost:$RANDOM_PORT"
+SERVER_ADDRESS_REMOTE="$SLURM_JOB_NODELIST:$RANDOM_PORT"
 
-export NEPTUNE_PROJECT_NAME="do-not-be-hasty/tmp"
+echo $SLURM_JOB_NODELIST
 
-SERVER_ADDRESS="localhost:$((49152 + RANDOM % (65535 - 49152)))"
+ACTOR_BINARY="python3 ../${ENVIRONMENT}/${AGENT}_main.py --run_mode=actor --nonce=${NONCE} --server_address=$SERVER_ADDRESS_LOCAL --mrunner_config=$EXPDIR/$MRUNER_CONFIG";
+LEARNER_BINARY="python3 ../${ENVIRONMENT}/${AGENT}_main.py --run_mode=learner --nonce=${NONCE} --server_address=$SERVER_ADDRESS_LOCAL --mrunner_config=$EXPDIR/$MRUNER_CONFIG";
 
-ACTOR_BINARY="python3 ../${ENVIRONMENT}/${AGENT}_main.py --run_mode=actor --nonce=${NONCE} --server_address=$SERVER_ADDRESS --mrunner_config=$EXPDIR/$MRUNER_CONFIG";
-LEARNER_BINARY="python3 ../${ENVIRONMENT}/${AGENT}_main.py --run_mode=learner --nonce=${NONCE} --server_address=$SERVER_ADDRESS --mrunner_config=$EXPDIR/$MRUNER_CONFIG";
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "running..."
+echo "-------"
+echo $ACTOR_BINARY
+echo $LEARNER_BINARY
 
-rm /tmp/agent -Rf
-${LEARNER_BINARY} --logtostderr --pdb_post_mortem --num_envs=${NUM_ACTORS} &
-LEARNER_PID=$!
+echo "TASK NO"
+echo $SLURM_STEP_ID
 
-for ((id=0; id<$NUM_ACTORS; id++)); do
-    echo "run actor"
-    ${ACTOR_BINARY} --logtostderr --pdb_post_mortem --num_envs=${NUM_ACTORS} --task=${id} &
-done
+echo "SLURM_JOB_NODELIST"
+echo $SLURM_JOB_NODELIST
 
-while kill -0 $LEARNER_PID
-do
-    sleep 60
-done
+NUM_ACTORS=$((MRUNNER_NTASKS - 1))
+echo "NUM_ACTORS"
+echo $NUM_ACTORS
+
+if [ 0 -eq $SLURM_STEP_ID ];
+then
+    echo "Running the learner"
+    ${LEARNER_BINARY} --logtostderr --num_envs=${NUM_ACTORS}
+else
+  TASK_ID=$((SLURM_STEP_ID - 1))
+  echo "Running an actor $TASK_ID"
+  ${ACTOR_BINARY} --logtostderr --num_envs=${NUM_ACTORS} --task=$TASK_ID
+fi
+
