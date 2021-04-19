f45cb7354cfcff6f97b5839a2448cc2519a61941
Michał <michal.zawalski25@gmail.com>
2021-04-19 21:50:19 +0200

    Fix extracting server address.

--- get_learner_node.py
+++ get_learner_node.py
@@ -5,6 +5,6 @@ nodelist = os.environ['SLURM_JOB_NODELIST']
 if "[" in nodelist:
   x = nodelist.find("[")
   nodes_nums = nodelist[x+1:-1]
-  print(nodelist[:x] + nodes_nums.split(",")[0])
+  print(nodelist[:x] + nodes_nums.split(",")[0].split("-")[0])
 else:
   print(nodelist)
